d40331ad80edf9a6e128998097dbeb276bcdfe22
RASULI Hamayon <hamayon.rasuli@soprasteria.com>
2017-02-07 13:43:25 +0000

    updated the query to GET MIN PRICE

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+'use strict';
+console.log('LOADING FUNCTION');
 var express = require('express');
 var mysql = require('mysql');
 
 
-var connection = mysql.createConnection({
+// exports.handler = function(event, context, callback) {
+        var connection = mysql.createConnection({
         host     : 'htdham.ciesfzx5ohll.eu-west-2.rds.amazonaws.com',
         user     : 'HTD',
         password : 'London123',
@@ -19,15 +22,25 @@ connection.connect(function(err) {
         return;
         }
 });
-
-
-
-connection.query('SELECT * FROM AlexaDB.Mobile WHERE model = "Iphone 7"',function(err,rows){
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    //console.log('value1 =', event.key1);
+   //this will get the cheapest Iphone 7
+connection.query('SELECT make , price FROM AlexaDB.Mobile WHERE model = "Iphone 7" AND price = (SELECT MIN(price) FROM AlexaDB.Mobile)',function(err,rows){
   if(err) throw err;
-
-  console.log('Data received from Db:\n');
   console.log(rows);
-});
+console.log(rows[0].make);
+console.log(rows[0].price);
+
+  var cheapestPhone = rows[0].price;
+  var output = 'The cheapest Iphone 7 is '+ cheapestPhone;
+  console.log(output);
+
 connection.end(function(error){
     //END CONNECTION
 })
+
+//     callback(null, output); // Echo back the first key value
+    // or
+    // callback("some error type"); 
+    });
+// };
